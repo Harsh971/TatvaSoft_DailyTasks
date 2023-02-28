48ea07379386d3da2ba55595429ba900417de900
Harsh971 <htk717716@gmail.com>
2023-02-28 18:27:23 +0530

    Day 5 Task 1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 import './App.css';
 import Home from './pages/Home';
 import Counter from './component/Counter';
+import EventBinding from './component/EventBinding';
+import About from './component/About';
+
 function App() {
   return (
     <div className='App'>
       <Home/>
-      <Counter/>
+      {/* <Counter/> */}
     </div>
     
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,45 @@
 import React from 'react'
+import {About} from '../component/About'
 import {Title} from '../component/Title'
 
 class Home extends React.Component {
-    constructor() {
-        super() 
-        this.state = {
-            title : 'Hello',
-            descripton : 'This is Description'
+
+    constructor(props){
+        super(props)
+        this.state={
+            // Change this to TRUE to go to About Page
+          isLoggedIn:false
         }
     }
-    render() {
-        return (
-            <>
-                {/* <h1>{this.state.title}</h1>
-                <h1>{this.state.descripton}</h1> */}
 
-                <Title heading={this.state.title} desc={this.state.descripton}></Title>
+    // constructor() {
+    //     super() 
+    //     this.state = {
+    //         title : 'Hello',
+    //         descripton : 'This is Description'
+    //     }
+    // }
 
-                {/* <Title headerContent="Harsh"/>
-                <Title headerContent="World"/>
-                <Title headerContent="People"/> */}
-            </>
-        )
-    }
+    // render() {
+    //     return (
+    //         <>
+    //             {/* <h1>{this.state.title}</h1>
+    //             <h1>{this.state.descripton}</h1> */}
+
+    //             <Title heading={this.state.title} desc={this.state.descripton}></Title>
+
+    //             {/* <Title headerContent="Harsh"/>
+    //             <Title headerContent="World"/>
+    //             <Title headerContent="People"/> */}
+    //         </>
+    //     )
+    // }
+
+    // Day 5
+    render()
+    {
+        <p>This is Home Page</p>
+        return(this.state.isLoggedIn ? <h1><About/></h1> : <h1>This is Home page</h1>)
+    }    
 }
 export default Home
